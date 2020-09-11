917aaa4c6302ea93f44ef5aab72dbed551aa529b
120006994 <120006994@NGLEDU-043.EDU.Local>
2020-09-11 15:03:50 +0200

    MainView added

--- src/pathfinding/Main.java
+++ src/pathfinding/Main.java
@@ -6,11 +6,7 @@
 package pathfinding;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 /**
@@ -20,25 +16,12 @@ import javafx.stage.Stage;
 public class Main extends Application {
     
     @Override
-    public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
+    public void start(Stage stage) {
+        MainView mainView = new MainView();
+        Scene scene = new Scene(mainView, 640, 480);
+        stage.setScene(scene);
+        stage.show();
         
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        
-        Scene scene = new Scene(root, 300, 250);
-        
-        primaryStage.setTitle("Hello World!");
-        primaryStage.setScene(scene);
-        primaryStage.show();
     }
 
     /**
