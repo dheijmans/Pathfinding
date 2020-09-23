25dfc99138cc8224200229c492b1c38c473e585c
120006678 <120006678@NGLEDU-039.EDU.Local>
2020-09-23 14:39:48 +0200

    Toolbar added

---
+++ src/pathfinding/Toolbar.java
@@ -0,0 +1,23 @@
+package pathfinding;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.ToolBar;
+
+public class Toolbar extends ToolBar {
+    
+    private final MainView mainView;
+    
+    public Toolbar(MainView mainView) {
+        this.mainView = mainView;
+
+        Button runButton = new Button("Run");
+        runButton.setOnAction(this::handleRun);
+        this.getItems().addAll(runButton);
+    }
+    
+    private void handleRun(ActionEvent event) {
+        this.mainView.getPathfinder().aStar();
+        this.mainView.draw();
+    }
+
+}
