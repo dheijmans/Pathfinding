d4485be1c668c3898ff0f7a11d3949e2ba9763cd
120006678 <120006678@NGLEDU-039.EDU.Local>
2020-11-23 13:08:21 +0100

    Legend added

--- src/pathfinding/Sidebar.java
+++ src/pathfinding/Sidebar.java
@@ -3,15 +3,18 @@ package pathfinding;
 import javafx.geometry.Pos;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
 
 public class Sidebar extends VBox {
     
+    private Label stats;
     private Label steps;
     private Label startNode;
     private Label endNode;
-    private Label blockedNodes;
-    private Label stats;
+    private Label blockedNodes;     
     
     private final MainView mainView;
     private final Pathfinder pathfinder;
@@ -22,18 +25,27 @@ public class Sidebar extends VBox {
         this.mainView = mv;
         this.pathfinder = this.mainView.getPathfinder();
         
+        this.stats = new Label("Statistics");
+        this.stats.setFont(new Font(20));
         this.steps = new Label("Amount of steps: -");
         this.startNode = new Label("Startnode: (0,0)");
         this.endNode = new Label("Endnode: (0,0)");
         this.blockedNodes = new Label("Amount of blocked nodes: 0");
-        this.stats = new Label("Statistics");
+                
+        Text block = new Text("■");
+               
+        TextFlow start = new TextFlow();
+        block.setFill(Color.GOLD);
+        Text startTxt = new Text(" - Start");
+        start.getChildren().addAll(block, startTxt);
+
         
-        this.stats.setFont(new Font(20));
+        
         
         this.totalBlocked = 0;
         
         this.setAlignment(Pos.TOP_LEFT);
-        this.getChildren().addAll(this.stats, this.steps, this.startNode, this.endNode, this.blockedNodes);          
+        this.getChildren().addAll(this.stats, this.steps, this.startNode, this.endNode, this.blockedNodes, start);          
     }
     
     public void setAmountOfSteps(int s) {
